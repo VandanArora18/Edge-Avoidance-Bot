e8e37678dd43a1f79ff88dc99d025806ffd6f44b
Vandan Arora <aroravandan337@gmail.com>
2026-09-23 06:10:26 +0530

    Add files via upload

---
+++ build/bot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/user/bot_ws/install/bot_controller/share/bot_controller/package.sh"
+. "/home/user/bot_ws/install/bot_description/share/bot_description/package.sh"
+. "/home/user/bot_ws/install/bot_script/share/bot_script/package.sh"
